11bf3f716cec4000ec75ad18b9e9991e3fa81ff2
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-02-11 11:19:40 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-    <button v-on:click="add_last()">追加</button>
+    <!--button v-on:click="add_last()">追加</button-->
     <div class="hello">
         <draggable v-model="data" group="people" item-key="id" tag="ul">
             <template #item="{element, index}">
-              <div>
+              <div style="display:flex;margin:10px">
                 <li @click="liclick(index)">{{element.id}}
+                  {{element.active}}
+                </li>
+                <li>
                   <span class="del" v-on:click="del(index)">[削除]</span>
+                </li>
+                <li>
                   <span class="add" v-on:click="add(index)">[追加]</span>
                 </li>
               </div>
@@ -26,25 +31,41 @@ export default {
             data: [
                 {
                     id: 1,
+                    active: 1,
                     content: "テスト1",
                 },
                 {
                     id: 2,
+                    active: 0,
                     content: "テスト2",
                 },
                 {
                     id: 3,
+                    active: 0,
                     content: "テスト3",
                 },
             ],
         };
     },
     methods: {
+      active(index) {
+        for (var i=0;i<this.data.length;i++)
+        {
+          this.data[i]["active"] = 0; 
+        }
 
+        this.data[index]["active"] = 1; 
+
+        for (i=0;i<this.data.length;i++)
+        {
+          console.log(this.data[i]["active"]); 
+        }
+      },
       add_last() {
         this.data.push( 
           {
               id: 3,
+              active: 0,
               content: "テスト3",
           }
         );
@@ -53,24 +74,20 @@ export default {
         this.data.splice(index+1, 0, 
           {
               id: 3,
+              active: 0,
               content: "テスト3",
           }
         );
+        this.active(index+1)
       },
       del(index) {
         this.data.splice(index, 1);
       },
-      liclick() {
+      liclick(index) {
         console.log("test")
-        this.data.push( 
-          {
-              id: 3,
-              content: "テスト3",
-          }
-        );
+        this.active(index)
       },
     },
-    
 };
 </script>
 
