c803d417275eee1c7d2e43f79091a688f6136247
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-02-11 09:47:31 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="add_last()">追加</button>
+    <div class="hello">
+        <draggable v-model="data" group="people" item-key="id" tag="ul">
+            <template #item="{element, index}">
+              <div>
+                <li>{{element.id}}
+                  <span class="del" v-on:click="del(index)">[削除]</span>
+                  <span class="add" v-on:click="add(index)">[追加]</span>
+                </li>
+              </div>
+            </template>
+        </draggable>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from "vuedraggable";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    components: {
+        draggable,
+    },
+    data() {
+        return {
+            data: [
+                {
+                    id: 1,
+                    content: "テスト1",
+                },
+                {
+                    id: 2,
+                    content: "テスト2",
+                },
+                {
+                    id: 3,
+                    content: "テスト3",
+                },
+            ],
+        };
+    },
+    methods: {
+
+      add_last() {
+        console.log("test")
+        this.data.push( 
+          {
+              id: 3,
+              content: "テスト3",
+          }
+        );
+      },
+      add(index) {
+        console.log("test")
+        this.data.splice(index+1, 0, 
+          {
+              id: 3,
+              content: "テスト3",
+          }
+        );
+      },
+      del(index) {
+        console.log("test")
+        this.data.splice(index, 1);
+      },
+
+    },
+    
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+ul {
+  list-style-type: none;
+}
+li {
+  cursor: pointer;
+  padding: 10px;
+  border: solid #ddd 1px;
 }
 </style>
